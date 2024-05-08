dfcdb4558aa35f562a2cd38e946f85d590b7f13e
Gabriele Giuseppe Antonio Satta <g.satta8@phd.uniss.it>
2024-05-08 15:27:16 +0200

    d

--- FLUO.R
+++ FLUO.R
@@ -39,15 +39,17 @@ fluo_plot
 # Save the graphs in png
 ggsave("./GRAPHS/fluo_plot.png", plot = fluo_plot, width = 8, height = 6, dpi = 300)
 
+# boxplot della produzione per la varietà
+boxplot(`Fv/Fm` ~ inoculum + treatment, data=fluo, col=c("red","green","pink","yellow"))
 
+################################################################################
 
-# 
+# Convert to factor the columns inoculum and treatments
 fluo$inoculum <- as.factor(fluo$inoculum)
 fluo$treatment <- as.factor(fluo$treatment)
 
-
 # Fittiamo il modello
-mod <- lm(`Fv/Fm` ~ inoculum + treatment, data = fluo)
+mod <- lm(`Fv/Fm` ~ inoculum + treatment + inoculum:treatment , data = fluo)
 
 # Visual check dei dati
 plot(mod, which = 1)
@@ -61,24 +63,31 @@ fluo <- fluo %>%
 # Controllare la normalità dei residui: shapiro test
 shapiro.test(mod$residuals)
 
-# Esegui l'analisi della varianza (ANOVA)
-anova_result <- aov(`Fv/Fm` ~ inoculum + treatment, data = fluo)
+# Shapiro-Wilk normality test
+# 
+# data:  mod$residuals
+# W = 0.82293, p-value = 1.209e-12
 
+# Esegui l'analisi della varianza (ANOVA)
+anova_result <- aov(`Fv/Fm` ~ inoculum + treatment + inoculum:treatment, data = fluo)
 
 summary(anova_result)
 
-# Df  Sum Sq  Mean Sq F value Pr(>F)  
-# inoculum      1 0.00664 0.006644   5.786 0.0173 *
-#   treatment     1 0.00239 0.002395   2.086 0.1507  
-# Residuals   157 0.18027 0.001148                 
+# Df  Sum Sq  Mean Sq F value
+# inoculum             1 0.00664 0.006644   5.773
+# treatment            1 0.00239 0.002395   2.081
+# inoculum:treatment   1 0.00075 0.000753   0.654
+# Residuals          156 0.17952 0.001151        
+# Pr(>F)  
+# inoculum           0.0174 *
+#   treatment          0.1511  
+# inoculum:treatment 0.4199  
+# Residuals                  
 # ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#   Signif. codes:  
+#   0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-# Non c'è divverenza significativa tra i tretamnet (pvalue = 0.15), invece 
-# c'è tra gli inoculum (pvalue 0.017)
-
-
-
+# Non c'è divverenza significativa 
 
 # Esegui il test di Scheffé (LSD) per confronti multipli
 lsd_result <- TukeyHSD(anova_result)
@@ -89,40 +98,31 @@ print(lsd_result)
 # Tukey multiple comparisons of means
 # 95% family-wise confidence level
 # 
-# Fit: aov(formula = `Fv/Fm` ~ inoculum + treatment, data = fluo)
+# Fit: aov(formula = `Fv/Fm` ~ inoculum + treatment + inoculum:treatment, data = fluo)
 # 
 # $inoculum
-# diff         lwr          upr    p adj
-# PC-NI -0.0128875 -0.02347016 -0.002304843 0.017319
+# diff         lwr          upr     p adj
+# PC-NI -0.0128875 -0.02348237 -0.002292633 0.0174481 # Ci sono differenze tra gli inoculi
 # 
 # $treatment
 # diff         lwr         upr     p adj
-# T-NT -0.0077375 -0.01832016 0.002845157 0.1506873
+# T-NT -0.0077375 -0.01833237 0.002857367 0.1511478
+# 
+# $`inoculum:treatment`
+# diff        lwr           upr     p adj
+# PC:NT-NI:NT -0.008550 -0.0282489  0.0111488963 0.6733471
+# NI:T-NI:NT  -0.003400 -0.0230989  0.0162988963 0.9698821
+# PC:T-NI:NT  -0.020625 -0.0403239 -0.0009261037 0.0362874 # Questo è significativo... differenza tra PC-T e NI-NT 
+# NI:T-PC:NT   0.005150 -0.0145489  0.0248488963 0.9049623
+# PC:T-PC:NT  -0.012075 -0.0317739  0.0076238963 0.3862346
+# PC:T-NI:T   -0.017225 -0.0369239  0.0024738963 0.1093765
 
 
+# I p values indicano che ci sono delle differenze significative tra 
+# gli inoculum e non tra i trattamenti
 
+library(multcompView)
 
+# Visualizza graficamente i risultati del test di Scheffé (LSD)
+plot(lsd_result)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-spad_0 <- readxl::read_excel("./FLUO_SPAD/SPAD.xlsx")
-str(spad_0)
--- SPAD.R
+++ SPAD.R
@@ -41,9 +41,108 @@ spad_plot <- ggplot(spad_summary, aes(x = Date, y = mean_spad, group = interacti
 
 spad_plot
 
-
 # Save the graphs in png
 ggsave("./GRAPHS/spad_plot.png", plot = spad_plot, width = 8, height = 6, dpi = 300)
+
+################################################################################
+
+# Convert to factor the columns inoculum and treatments
+spad$inoculum <- as.factor(spad$inoculum)
+spad$treatment <- as.factor(spad$treatment)
+
+# Fittiamo il modello
+mod <- lm(Value ~ inoculum + treatment, data = spad)
+
+# Visual check dei dati
+plot(mod, which = 1)
+
+plot(mod, which = 2)
+
+# aggiungiamo colonna dei residui
+spad <- spad %>%
+  mutate(residuals = residuals(mod))
+
+# Controllare la normalità dei residui: shapiro test
+shapiro.test(mod$residuals)
+
+# 	Shapiro-Wilk normality test
+# 
+# data:  mod$residuals
+# W = 0.96608, p-value = 0.0005786
+
+# Esegui l'analisi della varianza (ANOVA)
+anova_result <- aov(Value ~ inoculum + treatment + inoculum:treatment, data = spad)
+
+summary(anova_result)
+
+# Df Sum Sq Mean Sq F value Pr(>F)  
+# inoculum             1    142  141.94   6.522 0.0116 *
+#   treatment            1      0    0.26   0.012 0.9124  
+# inoculum:treatment   1      4    3.69   0.170 0.6811  
+# Residuals          156   3395   21.76                 
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# Non c'è divverenza significativa tra i treatments (pvalue = 0.9124), 
+# non c'è nell'interazione tra inoculum:treatment , invece 
+# c'è tra gli inoculum (pvalue 0.0116)
+
+# Esegui il test di Scheffé (LSD) per confronti multipli
+lsd_result <- TukeyHSD(anova_result)
+
+# Mostra i risultati del test di Scheffé (LSD)
+print(lsd_result)
+
+# > # Mostra i risultati del test di Scheffé (LSD)
+#   > print(lsd_result)
+# Tukey multiple comparisons of means
+# 95% family-wise confidence level
+# 
+# Fit: aov(formula = Value ~ inoculum + treatment + inoculum:treatment, data = spad)
+# 
+# $inoculum
+# diff       lwr        upr     p adj
+# PC-NI -1.88375 -3.340746 -0.4267537 0.0116124 # Ci sono differenze tra l'PC e NI
+# 
+# $treatment
+# diff       lwr      upr     p adj
+# T-NT 0.08125 -1.375746 1.538246 0.9124298 # Non ci sono differenze
+# 
+# $`inoculum:treatment`
+# diff       lwr      upr     p adj                     # non ci sono differenze
+# PC:NT-NI:NT -1.5800 -4.288974 1.128974 0.4312026
+# NI:T-NI:NT   0.3850 -2.323974 3.093974 0.9827710
+# PC:T-NI:NT  -1.8025 -4.511474 0.906474 0.3127312
+# NI:T-PC:NT   1.9650 -0.743974 4.673974 0.2392427
+# PC:T-PC:NT  -0.2225 -2.931474 2.486474 0.9965580
+# PC:T-NI:T   -2.1875 -4.896474 0.521474 0.1586047
+
+
+# Visualizza graficamente i risultati del test di Scheffé (LSD)
+plot(lsd_result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
