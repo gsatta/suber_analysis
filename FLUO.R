86e6763c33938903fff24daa4db8f2c196ade63d
Gabriele Giuseppe Antonio Satta <g.satta8@phd.uniss.it>
2024-05-08 12:10:33 +0200

    d

---
+++ FLUO.R
@@ -0,0 +1,13 @@
+################################################################################
+
+#                     Analisi Fluorescenza e Spad
+
+################################################################################
+
+# Load the necessary packages
+library(xml2); 
+
+# Load the dataframes
+fluo_0 <- readxl::read_excel("./FLUO_SPAD/Fluometer_quercus_seconda_prova.xlsx")
+spad_0 <- readxl::read_excel("./FLUO_SPAD/SPAD.xlsx")
+
